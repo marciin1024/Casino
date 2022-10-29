d465d70faf86bb46c46aff46b4b52e903c923e1d
Marcin Czarnecki <marcin.czarnecki1024@gmail.com>
2022-10-29 18:38:29 +0200

    Add project files.

---
+++ Casino/Guy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+        Random random = new Random();
+        /// <summary>
+        /// Wyświetla w konsoli imię i posiadaną kwotę.
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " ma " + Cash + " zł.");
+        }
+        /// <summary>
+        /// Przekazuje pieniądze i usuwa je z portfela (lub wyświetla w konsoli
+        /// komunikat o braku środków)
+        /// </summary>
+        /// <param name="amount">Przekazywana kwota.</param>
+        /// <returns>Ilość pieniędzy wyjmowanych z portfela lub 0, jeśli 
+        /// dostępne środki są za małe(lub parametr amount jest nieprawidłowy).</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " mówi: " + amount + " nie jest poprawną kwotą.");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " mówi: nie mam wystarczających środków, aby dać Ci " + amount + " zł.");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+        /// <summary>
+        /// Przyjmuje pieniądze i dodaje je do portfela(lub wyświetla w konsoli
+        /// komunikat o nieprawidłowej kwocie).
+        /// </summary>
+        /// <param name="amount">Przyjmowana kwota.</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " mówi: nie przyjmę " + amount + "zł.");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+        /// <summary>
+        /// Odpowiada za przebieg gry w kasynie
+        /// </summary>
+        /// <param name="odds">Prawdopodobieństwo przegranej</param>
+        public void Playing(double odds)
+        {
+            if (Cash > 0)
+            {
+                Console.WriteLine("\nTeraz gra: " + Name);
+                WriteMyInfo();
+                Console.WriteLine("Prawdopodobieństwo przegranej wynosi: " + odds + ". Stawiana kwota: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = GiveCash(amount) * 2;
+                    if (pot > 0)
+                    {
+                        if (random.NextDouble() > odds)
+                        {
+                            ReceiveCash(pot);
+                            Console.WriteLine("Wygrałeś " + pot + "zł!!!!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Niestety, przegrałeś.");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wprowadź poprawną kwotę.");
+                }
+            }
+        }
+    }
+}
---
+++ Casino/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Casino
+{
+    class Program
+    {
+        public static Random random = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w kasynie. ");
+            Console.WriteLine("Wpowadź ilość graczy: ");
+            string gamers = Console.ReadLine();
+            if (int.TryParse(gamers, out int members))
+            {
+                if (members > 0)
+                {
+                    Guy[] players = new Guy[members];
+                    
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        players[i] = new Guy();
+                        players[i].Cash = random.Next(100, 201);
+                        players[i].Name = "Gracz " + (i + 1);
+                    }
+                    Console.WriteLine("Lista graczy:");
+                    for (int i = 0; i < players.Length; i++)
+                    {
+                        players[i].WriteMyInfo();
+                    }
+                    Console.WriteLine("Wprowadź prawdopodobieństwo przegranej. Skorzystaj z przecinka.");
+                    string probability = Console.ReadLine();
+
+                    if (double.TryParse(probability, out double odds))
+                    {
+                        while (IsCash(players))
+                        {
+                            int gamer;
+                            gamer = random.Next(1, members + 1);  
+                            players[gamer-1].Playing(odds);      
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wprowadź prawidłowe dane. (Być może zastosowałeś '.' zamiast ',')");
+                        for (int i = 0; i < players.Length; i++)
+                        {
+                            Console.WriteLine(players[i].Name + " ma " + players[0].Cash);
+                        }
+                    }
+                    Console.WriteLine("Kasyno zawsze wygrywa");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wprowadź poprawną liczbę graczy");
+            }
+        }
+        /// <summary>
+        /// Sprawdza czy jeszcze jakiś gracz ma pieniądze
+        /// </summary>
+        /// <param name="p">Tablica graczy</param>
+        /// <returns>Zwraca wartość logiczną w zależności od tego czy jakiś gracz ma jeszcze pieniądze</returns>
+        public static bool IsCash(Guy[] p)
+        {
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i].Cash > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
